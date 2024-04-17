61b4a270c3a0b968d017533938e988dabf76ca30
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-04-17 20:08:31 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import RegisterButton from './components/registerButton/registerButton';
-import ButtonLogin from './components/buttonLogin/buttonLogin';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <RegisterButton></RegisterButton>
-        <ButtonLogin></ButtonLogin>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
