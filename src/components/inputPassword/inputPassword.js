9e40f6ddfc1b27868ef422dffd6aeed7882ae128
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-04-17 19:52:54 -0300

    Password visibility

--- src/components/inputPassword/inputPassword.js
+++ src/components/inputPassword/inputPassword.js
@@ -1,13 +1,16 @@
 import Box from '@mui/material/Box';
 import { TextField } from "@mui/material";
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { Visibility } from '@mui/icons-material';
 import IconButton from '@mui/material/IconButton';
 import "./inputPassword.css"
 import { useState } from 'react';
 
 export default function InputPassword()
 {
-    
+    var [password, setPassword] = useState("text");
+
+    var visibility = <Visibility/>
 
     return (
         <div>
@@ -15,12 +18,12 @@ export default function InputPassword()
         <TextField
             label="Password"
             required
-            type="password"
+            type={password}
             InputProps=
             {{
                 endAdornment:(
-                <IconButton id="view" size="large" variant="outlined">
-                    <VisibilityOff/>
+                <IconButton onClick={() => setPassword((password === "text" ?  "password" : "text"))} id="view" size="large" variant="outlined">
+                    {password === "text" ? visibility = <Visibility/> : visibility = <VisibilityOff/>}
                 </IconButton>
                 )
             }}
