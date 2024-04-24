36d263eb9f7f56bf88726e226b4b23923ee18021
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-04-24 20:58:30 -0300

    user fixes

--- src/components/inputUser/inputUser.js
+++ src/components/inputUser/inputUser.js
@@ -3,14 +3,21 @@ import { Input, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mu
 import Box from '@mui/material/Box';
 
 
-export default function InputUser()
+export default function InputUser({onChange})
 {
+    const handleChange = (e) => 
+    {
+        const value = e.target.value
+        onChange(value)
+    }
+
     return (
         <div>
         <Box>
         <AccountCircle sx={{color:"grey", position:"absolute", left:"80.5vh", margin:"14px"}}/>
         <TextField 
             id="User"
+            onChange={handleChange}
             variant="outlined" 
             label="Username" 
             required
