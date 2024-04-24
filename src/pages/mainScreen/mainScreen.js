ae71cfd67e9fdc675001a53d770ebc14d86c5728
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-04-24 20:59:28 -0300

    main screen handlers calls

--- src/pages/mainScreen/mainScreen.js
+++ src/pages/mainScreen/mainScreen.js
@@ -1,28 +1,51 @@
 import { Container, Grid } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import InputUser from '../../components/inputUser/inputUser'
 import InputPassword from '../../components/inputPassword/inputPassword'
 import "./mainScreen.css"
 import ButtonLogin from '../../components/buttonLogin/buttonLogin'
 import RegisterButton from '../../components/registerButton/registerButton'
+import { axiosPostUser } from "../../service/UserService"
 
 
 export default function MainScreen()
 {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleUsernameChange = (value) => 
+    {
+        setUsername(value)
+    }
+
+    const handlePasswordChange = (value) => 
+    {
+        setPassword(value)
+    }
+
+    const handleSubmit = async() => 
+    {
+        await axiosPostUser(
+            {
+                username : username,
+                password : password
+            }
+        )
+    }
 
     return (
         <Container id="container">
             <Grid container spacing={2}>
             <Grid item xs={12}>
-                <InputUser/>
+                <InputUser onChange={handleUsernameChange}/>
             </Grid>
 
             <Grid item xs={12}>
-                <InputPassword/>
+                <InputPassword onChange={handlePasswordChange}/>
             </Grid>
 
             <Grid item xs={12}>
-                <RegisterButton/>
+                <RegisterButton onClick={handleSubmit}/>
             </Grid>
 
             <Grid item xs={12}>
